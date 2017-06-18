b67bc35f2a696e091f59083651348d71ed3d58bd
Joe Huss <detain@interserver.net>
2017-06-17 21:24:07 -0400

    updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -16,7 +16,7 @@ class Plugin {
 	public function __construct() {
 	}
 
-	public static function Hooks() {
+	public static function getHooks() {
 		return [
 			'licenses.load_processing' => [__CLASS__, 'Load'],
 			'licenses.settings' => [__CLASS__, 'Settings'],
