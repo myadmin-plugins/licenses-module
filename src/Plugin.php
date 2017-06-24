8768c9ed8bb66b7fd23761b2dce21a05c564e8c1
Joe Huss <detain@interserver.net>
2017-06-24 01:39:58 -0400

    updates to the activation handling for licenses module

--- src/Plugin.php
+++ src/Plugin.php
@@ -27,30 +27,33 @@ class Plugin {
 		$service = $event->getSubject();
 		$service->set_module('licenses')
 			->set_enable(function() {
-				$db2->query("update {$settings['TABLE']} set {$settings['PREFIX']}_status='active' where {$settings['PREFIX']}_id='{$db->Record[$settings['PREFIX'] . '_id']}'", __LINE__, __FILE__);
-				$GLOBALS['tf']->history->add($module, 'change_status', 'active', $db->Record[$settings['PREFIX'].'_id'], $db->Record[$settings['PREFIX'] . '_custid']);
+				$serviceInfo = $service->getServiceInfo();
+				$settings = get_module_settings($service->get_module());
+				$db = get_module_db($service->get_module());
+				$db->query("update {$settings['TABLE']} set {$settings['PREFIX']}_status='active' where {$settings['PREFIX']}_id='{$serviceInfo[$settings['PREFIX'] . '_id']}'", __LINE__, __FILE__);
+				$GLOBALS['tf']->history->add($module, 'change_status', 'active', $serviceInfo[$settings['PREFIX'].'_id'], $serviceInfo[$settings['PREFIX'] . '_custid']);
 				$smarty = new TFSmarty;
-				$smarty->assign('license_ip', $db->Record[$settings['PREFIX'] . '_ip']);
+				$smarty->assign('license_ip', $serviceInfo[$settings['PREFIX'] . '_ip']);
 				$smarty->assign('service_name', $service_name);
 				$email = $smarty->fetch('email/admin_email_license_created.tpl');
 				//$subject = $smarty->get_template_vars('subject');
-				$subject = 'New ' . $service_name . ' License Created ' . $db->Record[$settings['PREFIX'].'_ip'];
+				$subject = 'New ' . $service_name . ' License Created ' . $serviceInfo[$settings['PREFIX'].'_ip'];
 				$headers = '';
 				$headers .= 'MIME-Version: 1.0' . EMAIL_NEWLINE;
 				$headers .= 'Content-type: text/html; charset=UTF-8' . EMAIL_NEWLINE;
 				$headers .= 'From: ' . TITLE . ' <' . EMAIL_FROM . '>' . EMAIL_NEWLINE;
 				admin_mail($subject, $email, $headers, false, 'admin_email_license_created.tpl');
 			})->set_reactivate(function() {
-				myadmin_log($module, 'info', "PPP - {$service_name} - {$serviceTypes[$db->Record[$settings['PREFIX'] . '_type']]['services_category']}", __LINE__, __FILE__);
-				myadmin_log($module, 'info', str_replace("\n", '', var_export($serviceTypes[$db->Record[$settings['PREFIX'] . '_type']], true)), __LINE__, __FILE__);
-				activate_license($db->Record[$settings['PREFIX'] . '_id']);
-				$db2->query("update {$settings['TABLE']} set {$settings['PREFIX']}_status='active' where {$settings['PREFIX']}_id='{$db->Record[$settings['PREFIX'] . '_id']}'", __LINE__, __FILE__);
-				$GLOBALS['tf']->history->add($module, 'change_status', 'active', $db->Record[$settings['PREFIX'] . '_id'], $db->Record[$settings['PREFIX'] . '_custid']);
+				$serviceInfo = $service->getServiceInfo();
+				$settings = get_module_settings($service->get_module());
+				$db = get_module_db($service->get_module());
+				$db->query("update {$settings['TABLE']} set {$settings['PREFIX']}_status='active' where {$settings['PREFIX']}_id='{$serviceInfo[$settings['PREFIX'] . '_id']}'", __LINE__, __FILE__);
+				$GLOBALS['tf']->history->add($module, 'change_status', 'active', $serviceInfo[$settings['PREFIX'] . '_id'], $serviceInfo[$settings['PREFIX'] . '_custid']);
 				$smarty = new TFSmarty;
-				$smarty->assign('license_ip', $db->Record[$settings['PREFIX'] . '_ip']);
-				$smarty->assign('service_name', (isset($service_name) ? $service_name : $serviceTypes[$db->Record[$settings['PREFIX'] . '_type']]['services_name']));
+				$smarty->assign('license_ip', $serviceInfo[$settings['PREFIX'] . '_ip']);
+				$smarty->assign('service_name', (isset($service_name) ? $service_name : $serviceTypes[$serviceInfo[$settings['PREFIX'] . '_type']]['services_name']));
 				$email = $smarty->fetch('email/admin_email_license_reactivated.tpl');
-				$subject = $db->Record[$settings['TITLE_FIELD']].' '.$service_name.' '.$settings['TBLNAME'].' Re-Activated';
+				$subject = $serviceInfo[$settings['TITLE_FIELD']].' '.$service_name.' '.$settings['TBLNAME'].' Re-Activated';
 				$headers = '';
 				$headers .= 'MIME-Version: 1.0' . EMAIL_NEWLINE;
 				$headers .= 'Content-type: text/html; charset=UTF-8' . EMAIL_NEWLINE;
