fc7c5160e9fd609b2e434d4d360e05d3fd4ed9d9
Joe Huss <detain@interserver.net>
2017-06-24 01:47:23 -0400

    updates fixing a couple little issues/bugs in the activatoin code

--- src/Plugin.php
+++ src/Plugin.php
@@ -26,34 +26,38 @@ class Plugin {
 	public static function Load(GenericEvent $event) {
 		$service = $event->getSubject();
 		$service->set_module('licenses')
-			->set_enable(function() {
+			->set_enable(function($service) {
+				$module = $service->get_module();
+				$serviceTypes = run_event('get_service_types', false, $module);
 				$serviceInfo = $service->getServiceInfo();
-				$settings = get_module_settings($service->get_module());
-				$db = get_module_db($service->get_module());
+				$settings = get_module_settings($module);
+				$db = get_module_db($module);
 				$db->query("update {$settings['TABLE']} set {$settings['PREFIX']}_status='active' where {$settings['PREFIX']}_id='{$serviceInfo[$settings['PREFIX'] . '_id']}'", __LINE__, __FILE__);
 				$GLOBALS['tf']->history->add($module, 'change_status', 'active', $serviceInfo[$settings['PREFIX'].'_id'], $serviceInfo[$settings['PREFIX'] . '_custid']);
-				$smarty = new TFSmarty;
+				$smarty = new \TFSmarty;
 				$smarty->assign('license_ip', $serviceInfo[$settings['PREFIX'] . '_ip']);
-				$smarty->assign('service_name', $service_name);
+				$smarty->assign('service_name', $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name']);
 				$email = $smarty->fetch('email/admin_email_license_created.tpl');
 				//$subject = $smarty->get_template_vars('subject');
-				$subject = 'New ' . $service_name . ' License Created ' . $serviceInfo[$settings['PREFIX'].'_ip'];
+				$subject = 'New ' . $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name'] . ' License Created ' . $serviceInfo[$settings['PREFIX'].'_ip'];
 				$headers = '';
 				$headers .= 'MIME-Version: 1.0' . EMAIL_NEWLINE;
 				$headers .= 'Content-type: text/html; charset=UTF-8' . EMAIL_NEWLINE;
 				$headers .= 'From: ' . TITLE . ' <' . EMAIL_FROM . '>' . EMAIL_NEWLINE;
 				admin_mail($subject, $email, $headers, false, 'admin_email_license_created.tpl');
-			})->set_reactivate(function() {
+			})->set_reactivate(function($service) {
+				$module = $service->get_module();
+				$serviceTypes = run_event('get_service_types', false, $module);
 				$serviceInfo = $service->getServiceInfo();
-				$settings = get_module_settings($service->get_module());
-				$db = get_module_db($service->get_module());
+				$settings = get_module_settings($module);
+				$db = get_module_db($module);
 				$db->query("update {$settings['TABLE']} set {$settings['PREFIX']}_status='active' where {$settings['PREFIX']}_id='{$serviceInfo[$settings['PREFIX'] . '_id']}'", __LINE__, __FILE__);
 				$GLOBALS['tf']->history->add($module, 'change_status', 'active', $serviceInfo[$settings['PREFIX'] . '_id'], $serviceInfo[$settings['PREFIX'] . '_custid']);
-				$smarty = new TFSmarty;
+				$smarty = new \TFSmarty;
 				$smarty->assign('license_ip', $serviceInfo[$settings['PREFIX'] . '_ip']);
-				$smarty->assign('service_name', (isset($service_name) ? $service_name : $serviceTypes[$serviceInfo[$settings['PREFIX'] . '_type']]['services_name']));
+				$smarty->assign('service_name', $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name']);
 				$email = $smarty->fetch('email/admin_email_license_reactivated.tpl');
-				$subject = $serviceInfo[$settings['TITLE_FIELD']].' '.$service_name.' '.$settings['TBLNAME'].' Re-Activated';
+				$subject = $serviceInfo[$settings['TITLE_FIELD']].' '.$serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name'].' '.$settings['TBLNAME'].' Re-Activated';
 				$headers = '';
 				$headers .= 'MIME-Version: 1.0' . EMAIL_NEWLINE;
 				$headers .= 'Content-type: text/html; charset=UTF-8' . EMAIL_NEWLINE;
