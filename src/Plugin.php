219a828d11c964aa182194159dc4ef32a088c7d0
Joe Huss <detain@interserver.net>
2017-07-30 23:35:18 -0400

    refactoring some calls

--- src/Plugin.php
+++ src/Plugin.php
@@ -57,7 +57,7 @@ class Plugin {
 	public static function loadProcessing(GenericEvent $event) {
 		$service = $event->getSubject();
 		$service->setModule(self::$module)
-			->set_enable(function($service) {
+			->setEnable(function($service) {
 				$serviceTypes = run_event('get_service_types', FALSE, self::$module);
 				$serviceInfo = $service->getServiceInfo();
 				$settings = get_module_settings(self::$module);
@@ -75,7 +75,7 @@ class Plugin {
 				$headers .= 'Content-type: text/html; charset=UTF-8'.EMAIL_NEWLINE;
 				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.EMAIL_NEWLINE;
 				admin_mail($subject, $email, $headers, FALSE, 'admin_email_license_created.tpl');
-			})->set_reactivate(function($service) {
+			})->setReactivate(function($service) {
 				$serviceTypes = run_event('get_service_types', FALSE, self::$module);
 				$serviceInfo = $service->getServiceInfo();
 				$settings = get_module_settings(self::$module);
@@ -92,7 +92,7 @@ class Plugin {
 				$headers .= 'Content-type: text/html; charset=UTF-8'.EMAIL_NEWLINE;
 				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.EMAIL_NEWLINE;
 				admin_mail($subject, $email, $headers, FALSE, 'admin_email_license_reactivated.tpl');
-			})->set_disable(function() {
+			})->setDisable(function() {
 			})->register();
 	}
 
