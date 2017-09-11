db9f864b404638366b99866497f63906e4d61075
Joe Huss <detain@interserver.net>
2017-09-11 02:23:36 -0400

    updates to email template path

--- src/Plugin.php
+++ src/Plugin.php
@@ -67,14 +67,14 @@ class Plugin {
 				$smarty = new \TFSmarty;
 				$smarty->assign('license_ip', $serviceInfo[$settings['PREFIX'].'_ip']);
 				$smarty->assign('service_name', $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name']);
-				$email = $smarty->fetch('email/admin_email_license_created.tpl');
+				$email = $smarty->fetch('email/admin/license_created.tpl');
 				//$subject = $smarty->get_template_vars('subject');
 				$subject = 'New '.$serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name'].' License Created '.$serviceInfo[$settings['PREFIX'].'_ip'];
 				$headers = '';
 				$headers .= 'MIME-Version: 1.0'.EMAIL_NEWLINE;
 				$headers .= 'Content-type: text/html; charset=UTF-8'.EMAIL_NEWLINE;
 				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.EMAIL_NEWLINE;
-				admin_mail($subject, $email, $headers, FALSE, 'admin_email_license_created.tpl');
+				admin_mail($subject, $email, $headers, FALSE, 'admin/license_created.tpl');
 			})->setReactivate(function($service) {
 				$serviceTypes = run_event('get_service_types', FALSE, self::$module);
 				$serviceInfo = $service->getServiceInfo();
@@ -85,13 +85,13 @@ class Plugin {
 				$smarty = new \TFSmarty;
 				$smarty->assign('license_ip', $serviceInfo[$settings['PREFIX'].'_ip']);
 				$smarty->assign('service_name', $serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name']);
-				$email = $smarty->fetch('email/admin_email_license_reactivated.tpl');
+				$email = $smarty->fetch('email/admin/license_reactivated.tpl');
 				$subject = $serviceInfo[$settings['TITLE_FIELD']].' '.$serviceTypes[$serviceInfo[$settings['PREFIX'].'_type']]['services_name'].' '.$settings['TBLNAME'].' Re-Activated';
 				$headers = '';
 				$headers .= 'MIME-Version: 1.0'.EMAIL_NEWLINE;
 				$headers .= 'Content-type: text/html; charset=UTF-8'.EMAIL_NEWLINE;
 				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.EMAIL_NEWLINE;
-				admin_mail($subject, $email, $headers, FALSE, 'admin_email_license_reactivated.tpl');
+				admin_mail($subject, $email, $headers, FALSE, 'admin/license_reactivated.tpl');
 			})->setDisable(function() {
 			})->register();
 	}
