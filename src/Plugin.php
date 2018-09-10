524f44b7b40b7c94f174c3b313945c6c201c0e2b
Joe Huss <detain@interserver.net>
2018-09-10 02:53:04 -0400

    formatting updates

--- src/Plugin.php
+++ src/Plugin.php
@@ -9,8 +9,8 @@ use Symfony\Component\EventDispatcher\GenericEvent;
  *
  * @package Detain\MyAdminLicenses
  */
-class Plugin {
-
+class Plugin
+{
 	public static $name = 'Licensing';
 	public static $description = 'Allows selling of Licenses.';
 	public static $help = '';
@@ -18,8 +18,8 @@ class Plugin {
 	public static $type = 'module';
 	public static $settings = [
 		'SERVICE_ID_OFFSET' => 5000,
-		'USE_REPEAT_INVOICE' => TRUE,
-		'USE_PACKAGES' => TRUE,
+		'USE_REPEAT_INVOICE' => true,
+		'USE_PACKAGES' => true,
 		'BILLING_DAYS_OFFSET' => 0,
 		'IMGNAME' => 'certificate.png',
 		'REPEAT_BILLING_METHOD' => NORMAL_BILLING,
@@ -38,13 +38,15 @@ class Plugin {
 	/**
 	 * Plugin constructor.
 	 */
-	public function __construct() {
+	public function __construct()
+	{
 	}
 
 	/**
 	 * @return array
 	 */
-	public static function getHooks() {
+	public static function getHooks()
+	{
 		return [
 			self::$module.'.load_processing' => [__CLASS__, 'loadProcessing'],
 			self::$module.'.settings' => [__CLASS__, 'getSettings']
@@ -54,14 +56,15 @@ class Plugin {
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function loadProcessing(GenericEvent $event) {
+	public static function loadProcessing(GenericEvent $event)
+	{
 		/**
 		 * @var \ServiceHandler $service
 		 */
 		$service = $event->getSubject();
 		$service->setModule(self::$module)
-			->setEnable(function($service) {
-				$serviceTypes = run_event('get_service_types', FALSE, self::$module);
+			->setEnable(function ($service) {
+				$serviceTypes = run_event('get_service_types', false, self::$module);
 				$serviceInfo = $service->getServiceInfo();
 				$settings = get_module_settings(self::$module);
 				$db = get_module_db(self::$module);
@@ -77,9 +80,9 @@ class Plugin {
 				$headers .= 'MIME-Version: 1.0'.PHP_EOL;
 				$headers .= 'Content-type: text/html; charset=UTF-8'.PHP_EOL;
 				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.PHP_EOL;
-				admin_mail($subject, $email, $headers, FALSE, 'admin/license_created.tpl');
-			})->setReactivate(function($service) {
-				$serviceTypes = run_event('get_service_types', FALSE, self::$module);
+				admin_mail($subject, $email, $headers, false, 'admin/license_created.tpl');
+			})->setReactivate(function ($service) {
+				$serviceTypes = run_event('get_service_types', false, self::$module);
 				$serviceInfo = $service->getServiceInfo();
 				$settings = get_module_settings(self::$module);
 				$db = get_module_db(self::$module);
@@ -94,15 +97,16 @@ class Plugin {
 				$headers .= 'MIME-Version: 1.0'.PHP_EOL;
 				$headers .= 'Content-type: text/html; charset=UTF-8'.PHP_EOL;
 				$headers .= 'From: '.TITLE.' <'.EMAIL_FROM.'>'.PHP_EOL;
-				admin_mail($subject, $email, $headers, FALSE, 'admin/license_reactivated.tpl');
-			})->setDisable(function() {
+				admin_mail($subject, $email, $headers, false, 'admin/license_reactivated.tpl');
+			})->setDisable(function () {
 			})->register();
 	}
 
 	/**
 	 * @param \Symfony\Component\EventDispatcher\GenericEvent $event
 	 */
-	public static function getSettings(GenericEvent $event) {
+	public static function getSettings(GenericEvent $event)
+	{
 		$settings = $event->getSubject();
 		$settings->add_dropdown_setting(self::$module, 'General', 'outofstock_licenses', 'Out Of Stock Licenses', 'Enable/Disable Sales Of This Type', $settings->get_setting('OUTOFSTOCK_LICENSES'), ['0', '1'], ['No', 'Yes']);
 	}
